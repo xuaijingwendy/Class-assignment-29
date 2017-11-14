182c56b4fb1593837030a9ae1bd5570834c1b0c8
xuaijingwendy <xuaijingwendy@163.com>
2017-11-14 14:41:17 +0800

    Crawling web pages

---
+++ request =urllib2.Request()-rlopen(request).py
@@ -0,0 +1,103 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import urllib2
+>>> request =urllib2.request("https://www.baidu.com/")
+
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    request =urllib2.request("https://www.baidu.com/")
+AttributeError: 'module' object has no attribute 'request'
+>>> request =urllib2.Request("https://www.baidu.com/")
+>>> response=urllib2.urlopen(request)
+>>> print response.read()
+<html>
+<head>
+	<script>
+		location.replace(location.href.replace("https://","http://"));
+	</script>
+</head>
+<body>
+	<noscript><meta http-equiv="refresh" content="0;url=http://www.baidu.com/"></noscript>
+</body>
+</html>
+>>> print response.read(response)
+
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    print response.read(response)
+  File "C:\Python27\lib\socket.py", line 377, in read
+    left = size - buf_len
+TypeError: unsupported operand type(s) for -: 'instance' and 'int'
+>>>  print response.read(3000)
+ 
+  File "<pyshell#6>", line 2
+    print response.read(3000)
+    ^
+IndentationError: unexpected indent
+>>> print response.read(3000)
+
+>>> print response.read(3000)
+
+>>> print response.read(3000)
+
+>>> request =urllib2.Request("http://www.runoob.com/python/python-tutorial.html")
+>>> response=urllib2.urlopen(request)
+>>> print response.read(3000)
+<!Doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta property="qc:admins" content="465267610762567726375" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Python 基础教程 | 菜鸟教程</title>
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel="canonical" href="http://www.runoob.com/python/python-tutorial.html" />
+<meta name="keywords" content="Python 基础教程,Python">
+<meta name="description" content="Python 基础教程   Python是一种解释型、面向对象、动态数据类型的高级程序设计语言。 Python由Guido van Rossum于1989年底发明，第一个公开发行版发行于1991年。  像Perl语言一样, Python 源代码同样遵循 GPL(GNU General Public License)协议。 本教程主要针对Python 2.x版本的学习，如果你使用的是Python 3.x版本请移步至Python 3.X版本..">
+<link rel="shortcut icon" href="//static.runoob.com/images/favicon.ico" mce_href="//static.runoob.com/images/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="/wp-content/themes/runoob/style.css?v=1.14" type="text/css" media="all" />
+<link rel="stylesheet" href="//cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" media="all" />
+<!--[if gte IE 9]><!-->
+  <script src="//cdn.bootcss.com/jquery/2.0.3/jquery.min.js"></script>
+  <!--<![endif]-->
+<!--[if lt IE 9]>
+     <script src="//cdn.bootcss.com/jquery/1.9.1/jquery.min.js"></script>
+     <script src="//cdn.bootcss.com/html5shiv/r29/html5.min.js"></script>
+  <![endif]-->
+<link rel="apple-touch-icon" href="//static.runoob.com/images/icon/mobile-icon.png"/>
+<meta name="apple-mobile-web-app-title" content="菜鸟教程">
+</head>
+<body>
+<div class="container logo-search">
+<div class="col search row-search-mobile">
+<form action="index.php">
+<input class="placeholder" value="搜索……" name="s" autocomplete="off">
+</form>
+</div>
+<div class="row">
+<div class="col logo">
+<h1><a href="/">菜鸟教程 -- 学的不仅是技术，更是梦想！</a></h1>
+</div>
+<div class="col right-list">
+<button class="btn btn-responsive-nav btn-inverse" data-toggle="collapse" data-target=".nav-main-collapse" id="pull" style=""> <i class="fa fa-navicon"></i> </button>
+</div>
+<div class="col search search-desktop last">
+<form action="//www.runoob.com/" target="_blank">
+<input class="placeholder" id="s" name="s" value="搜索……" autocomplete="off">
+</form>
+</div>
+</div>
+</div>
+<div class="container navigation">
+<div class="row">
+<div class="col nav">
+<ul class="pc-nav">
+<li><a href="//www.runoob.com/">首页</a></li>
+<li><a href="/html/html-tutorial.html">HTML</a></li>
+<li><a href="/css/css-tutorial.html">CSS</a></li>
+<li><a href="/js/js-tutorial.html">JavaScript</a></li>
+<li><a href="/jquery/jquery-tutorial.html">jQuery</a></li>
+<li><a href="/bootstrap/bootstrap-tutorial.html">Bootstrap</a></li>
+<li><a href="/sql/sql-tutorial.html">SQL</a></li>
+<li><a href="/mysql/mysql-tutorial.html">MySQL</a>
+>>> 
---
+++ 网页.py
@@ -0,0 +1,130 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import urllib2
+>>> response=urllib2.open("http:://ggfw.wuxi.gov.cn/wx_portal/")
+
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    response=urllib2.open("http:://ggfw.wuxi.gov.cn/wx_portal/")
+AttributeError: 'module' object has no attribute 'open'
+>>> response=urllib2.urlopen("http:://ggfw.wuxi.gov.cn/wx_portal/")
+
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    response=urllib2.urlopen("http:://ggfw.wuxi.gov.cn/wx_portal/")
+  File "C:\Python27\lib\urllib2.py", line 154, in urlopen
+    return opener.open(url, data, timeout)
+  File "C:\Python27\lib\urllib2.py", line 427, in open
+    req = meth(req)
+  File "C:\Python27\lib\urllib2.py", line 1126, in do_request_
+    raise URLError('no host given')
+URLError: <urlopen error no host given>
+>>> response = urllib2.urlopen("http:://ggfw.wuxi.gov.cn/wx_portal/")
+
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    response = urllib2.urlopen("http:://ggfw.wuxi.gov.cn/wx_portal/")
+  File "C:\Python27\lib\urllib2.py", line 154, in urlopen
+    return opener.open(url, data, timeout)
+  File "C:\Python27\lib\urllib2.py", line 427, in open
+    req = meth(req)
+  File "C:\Python27\lib\urllib2.py", line 1126, in do_request_
+    raise URLError('no host given')
+URLError: <urlopen error no host given>
+>>> import urllib2
+>>> response=urllib2.urlopen("http:://ggfw.wuxi.gov.cn/wx_portal/")
+
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    response=urllib2.urlopen("http:://ggfw.wuxi.gov.cn/wx_portal/")
+  File "C:\Python27\lib\urllib2.py", line 154, in urlopen
+    return opener.open(url, data, timeout)
+  File "C:\Python27\lib\urllib2.py", line 427, in open
+    req = meth(req)
+  File "C:\Python27\lib\urllib2.py", line 1126, in do_request_
+    raise URLError('no host given')
+URLError: <urlopen error no host given>
+>>> reponse = urllib2.urlopen("http://ggfw.wuxi.gov.cn/wx_portal/")
+>>> print response.read(3000)
+
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    print response.read(3000)
+NameError: name 'response' is not defined
+>>> print response.read(3000)
+
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    print response.read(3000)
+NameError: name 'response' is not defined
+>>> print reponse.read(3000)
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>无锡市公共服务平台</title>
+
+<link rel="stylesheet" type="text/css" href="/wx_portal/index/B1/css/base.css">
+<link rel="stylesheet" type="text/css" href="/wx_portal/index/B1/css/header-wx.css">
+<link rel="stylesheet" type="text/css" href="/wx_portal/index/B1/css/selectform.css">
+<link rel="stylesheet" type="text/css" href="/wx_portal/index/B1/css/footer-wx.css">
+<link type="text/css" rel="stylesheet" href="/wx_portal/index/B1/css/owl.carousel.css">
+<link type="text/css" rel="stylesheet" href="/wx_portal/index/B1/css/owl.theme.css">
+<link rel="stylesheet" type="text/css" href="/wx_portal/index/B1/css/peo_index.css">
+<script src="/wx_portal/common/js/jquery-1.8.2.min.js"></script>
+<!-- <script type="text/javascript" src="/wx_portal/index/B1/js/jquery.js"></script> -->
+<script type="text/javascript">
+		var indexUserId = 'null';
+		var app_path='/wx_portal';
+		var bx_myserver_catalogid = '1001';
+// 		var mourl = "http://ggfw.wuxi.gov.cn/wx_mo";
+// 		var imageurl = "http://ggfw.wuxi.gov.cn/image";
+	</script>
+<link rel="stylesheet" href="/wx_portal/common/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/wx_portal/common/css/local_base.css" type="text/css"/>
+	<link rel="stylesheet" href="/wx_portal/frame/Bs1/css/local_common.css" />
+	<!--page层-->
+<!-- 	<link rel="stylesheet" href="/wx_portal/index/B1/css/city_index.css" type="text/css"/> -->
+	<script src="/wx_portal/common/js/bootstrap.min.js"></script>
+	<script src="/wx_portal/common/js/main.js"></script>	
+	<!-- <script src="/wx_portal/common/js/plugins/jquery.slides.min.js"></script> -->
+<!-- 	<script src="/wx_portal/common/js/plugins/jquery.nivo.slider.js"></script> -->
+	<script src="/wx_portal/frame/Bs1/js/local_frame.js"></script>
+	<script type="text/javascript" src="/wx_portal/common/js/common1.js"></script>
+	<script type="text/javascript" src="/wx_portal/js/jquery.cookie.js"></script>
+	<script type="text/javascript" src="/wx_portal/js/cookie.js"></script>
+	<script type="text/javascript" src="/wx_portal/js/delcookie.js"></script>
+	<script type="text/javascript" src="/wx_portal/index/B1/js/index.js"></script>
+	<script type="text/javascript" src="/wx_portal/index/B1/js/owl.carousel.min.js"></script>
+<script type="text/javascript">
+		$(function(){
+	//	    index.init();
+			//index.showRecommend(0);
+			index.getCatapplist(bx_myserver_catalogid);
+			index.getSpecial('1005');
+			$("#peoServiceBtn").addClass('selectedItem');
+			// 重置
+			$(".resetbtn").click(function(){
+				$(this).parent().parent().find("input[type='text']").val("");
+			});
+			// 轮播 
+	        $('#scroll').owlCarousel({
+				items: 7,
+				itemsDesktop:[2400,7],
+				itemsDesktopSmall:[800,7],
+				autoPlay: true,
+				navigation: true,
+				navigationText: ["",""],
+				scrollPerPage: true
+			});   
+	  });
+	</script>
+	<style type="text/css">
+	.ui_tumble_list a{
+		margin-bottom: 5px;
+
+>>> 
