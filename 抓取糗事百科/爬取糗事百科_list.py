9f8b96a4cfe4bafcd7809c588a05587fced0dc84
xuaijingwendy <xuaijingwendy@163.com>
2017-11-21 14:48:47 +0800

    qiushibaike

---
+++ 抓取糗事百科/爬取糗事百科_list.py
@@ -0,0 +1,25 @@
+import urllib
+import urllib2
+import re
+ 
+user_agent='Mozilla/4.0(compatible;MSIE 5.5;windows NT)'
+headers={'User-Agent':user_agent}
+
+
+page = 1
+url = 'http://www.qiushibaike.com/hot/page/' + str(page)
+try:
+    request = urllib2.Request(url,headers=headers)
+    response = urllib2.urlopen(request)
+    html=response.read()
+    print response.read(2000)
+except urllib2.URLError, e:
+    if hasattr(e,"code"):
+        print e.code
+    if hasattr(e,"reason"):
+        print e.reason
+
+content_pattern=re.compile('<div class="content">.*?<span>(.*?)</span>',re.S)
+content_list=re.findall(content_pattern,html)
+for item in content_list:
+    print item
